440645a8e6c2ec708e44af1a43a400e10221d195
mtraworld <kylewong@traworld.com>
2025-02-21 10:37:52 +0800

    feat!: Move highlights config into table
    While adding more built-in actions with different hlgroup, instead of
    listing out all of the options in config, I think moving into a table is
    a better choice. Please read the docs on how to set it up.
    
    No errors will be thrown if you're using old config. The colors will
    just fall back to default until you change them.

--- lua/undo-glow/config.lua
+++ lua/undo-glow/config.lua
@@ -5,20 +5,36 @@ local config = {
 	animation_type = "fade",
 	easing = require("undo-glow.easing").ease_in_out_cubic,
 	fps = 120,
-	undo_hl = "UgUndo",
-	redo_hl = "UgRedo",
-	yank_hl = "UgYank",
-	paste_below_hl = "UgPasteBelow",
-	paste_above_hl = "UgPasteAbove",
-	search_next_hl = "UgSearchNext",
-	search_prev_hl = "UgSearchPrev",
-	undo_hl_color = require("undo-glow.color").default_undo,
-	redo_hl_color = require("undo-glow.color").default_redo,
-	yank_hl_color = require("undo-glow.color").default_yank,
-	paste_below_hl_color = require("undo-glow.color").default_paste_below,
-	paste_above_hl_color = require("undo-glow.color").default_paste_above,
-	search_next_hl_color = require("undo-glow.color").default_search_next,
-	search_prev_hl_color = require("undo-glow.color").default_search_prev,
+	highlights = {
+		undo = {
+			hl = "UgUndo",
+			hl_color = require("undo-glow.color").default_undo,
+		},
+		redo = {
+			hl = "UgRedo",
+			hl_color = require("undo-glow.color").default_redo,
+		},
+		yank = {
+			hl = "UgYank",
+			hl_color = require("undo-glow.color").default_yank,
+		},
+		paste_below = {
+			hl = "UgPasteBelow",
+			hl_color = require("undo-glow.color").default_paste_below,
+		},
+		paste_above = {
+			hl = "UgPasteAbove",
+			hl_color = require("undo-glow.color").default_paste_above,
+		},
+		search_next = {
+			hl = "UgSearchNext",
+			hl_color = require("undo-glow.color").default_search_next,
+		},
+		search_prev = {
+			hl = "UgSearchPrev",
+			hl_color = require("undo-glow.color").default_search_prev,
+		},
+	},
 }
 
 return config
