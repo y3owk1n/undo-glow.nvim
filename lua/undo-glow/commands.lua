e222a386f32fa1b8a24975fa23e18e7aa5344a27
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-02-22 23:50:52 +0800

    feat: refactor with more util functions (#89)

--- lua/undo-glow/commands.lua
+++ lua/undo-glow/commands.lua
@@ -2,77 +2,45 @@ local M = {}
 
 ---@param opts? UndoGlow.CommandOpts
 function M.undo(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgUndo",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgUndo", opts)
+	require("undo-glow").highlight_changes(opts)
 	vim.cmd("undo")
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.redo(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgRedo",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgRedo", opts)
+	require("undo-glow").highlight_changes(opts)
 	vim.cmd("redo")
 end
 
 --- Helper to use this in autocmds. Do not use this as a command, it does nothing.
 ---@param opts? UndoGlow.CommandOpts
 function M.yank(opts)
-	opts = opts or {}
+	opts = require("undo-glow.utils").merge_command_opts("UgYank", opts)
+
 	local pos = vim.fn.getpos("'[")
 	local pos2 = vim.fn.getpos("']")
-	require("undo-glow").highlight_region({
-		hlgroup = opts.hlgroup or "UgYank",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
+
+	require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
 		s_row = pos[2] - 1,
 		s_col = pos[3] - 1,
 		e_row = pos2[2] - 1,
 		e_col = pos2[3],
-	})
+	}))
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.paste_below(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgPaste",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgPaste", opts)
+	require("undo-glow").highlight_changes(opts)
 	vim.cmd("normal! p")
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.paste_above(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgPaste",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgPaste", opts)
+	require("undo-glow").highlight_changes(opts)
 	vim.cmd("normal! P")
 end
 
@@ -88,20 +56,14 @@ function M.search_next(opts)
 		return
 	end
 
-	opts = opts or {}
+	opts = require("undo-glow.utils").merge_command_opts("UgSearch", opts)
 
-	require("undo-glow").highlight_region({
-		hlgroup = opts.hlgroup or "UgSearch",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
+	require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
 		s_row = region.s_row,
 		s_col = region.s_col,
 		e_row = region.e_row,
 		e_col = region.e_col,
-	})
+	}))
 end
 
 ---@param opts? UndoGlow.CommandOpts
@@ -117,20 +79,14 @@ function M.search_prev(opts)
 		return
 	end
 
-	opts = opts or {}
+	opts = require("undo-glow.utils").merge_command_opts("UgSearch", opts)
 
-	require("undo-glow").highlight_region({
-		hlgroup = opts.hlgroup or "UgSearch",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
+	require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
 		s_row = region.s_row,
 		s_col = region.s_col,
 		e_row = region.e_row,
 		e_col = region.e_col,
-	})
+	}))
 end
 
 ---@param opts? UndoGlow.CommandOpts
@@ -145,61 +101,34 @@ function M.search_star(opts)
 		return
 	end
 
-	opts = opts or {}
+	opts = require("undo-glow.utils").merge_command_opts("UgSearch", opts)
 
-	require("undo-glow").highlight_region({
-		hlgroup = opts.hlgroup or "UgSearch",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
+	require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
 		s_row = region.s_row,
 		s_col = region.s_col,
 		e_row = region.e_row,
 		e_col = region.e_col,
-	})
+	}))
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.comment(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgComment",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgComment", opts)
+	require("undo-glow").highlight_changes(opts)
 	return require("vim._comment").operator()
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.comment_textobject(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgComment",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgComment", opts)
+	require("undo-glow").highlight_changes(opts)
 	return require("vim._comment").textobject()
 end
 
 ---@param opts? UndoGlow.CommandOpts
 function M.comment_line(opts)
-	opts = opts or {}
-	require("undo-glow").highlight_changes({
-		hlgroup = opts.hlgroup or "UgComment",
-		animation_type = opts.animation_type,
-		animation = opts.animation,
-		duration = opts.duration,
-		easing = opts.easing,
-		fps = opts.fps,
-	})
+	opts = require("undo-glow.utils").merge_command_opts("UgComment", opts)
+	require("undo-glow").highlight_changes(opts)
 	return require("vim._comment").operator() .. "_"
 end
 
