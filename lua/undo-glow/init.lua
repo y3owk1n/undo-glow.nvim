fdf13b6e4a378d0014e58bf49ef94ceb544a9918
y3owk1n <wongyeowkin@gmail.com>
2025-02-24 22:50:43 +0800

    feat: restructure public APIs

--- lua/undo-glow/init.lua
+++ lua/undo-glow/init.lua
@@ -66,63 +66,8 @@ local M = {}
 ---@field e_row integer End row
 ---@field e_col integer End column
 
-M.config = require("undo-glow.config")
 M.easing = require("undo-glow.easing")
-
-M.undo = require("undo-glow.commands").undo
-M.redo = require("undo-glow.commands").redo
-M.yank = require("undo-glow.commands").yank
-M.paste_below = require("undo-glow.commands").paste_below
-M.paste_above = require("undo-glow.commands").paste_above
-M.search_next = require("undo-glow.commands").search_next
-M.search_prev = require("undo-glow.commands").search_prev
-M.search_star = require("undo-glow.commands").search_star
-M.comment = require("undo-glow.commands").comment
-M.comment_textobject = require("undo-glow.commands").comment_textobject
-M.comment_line = require("undo-glow.commands").comment_line
-
-local highlights = require("undo-glow.highlight")
-local callback = require("undo-glow.callback")
-local utils = require("undo-glow.utils")
-
--- Helper to attach to a buffer with a local state.
----@param opts? UndoGlow.HighlightChanges|UndoGlow.CommandOpts
-function M.highlight_changes(opts)
-	local bufnr = vim.api.nvim_get_current_buf()
-
-	local state = utils.create_state(opts, M.config)
-
-	vim.api.nvim_buf_attach(bufnr, false, {
-		on_bytes = function(...)
-			return callback.on_bytes_wrapper(state, M.config, ...)
-		end,
-	})
-end
-
---- Highlight a specified region in the current buffer.
---- This API can be used for yanking or any other use case where you want to
---- temporarily highlight a region without modifying the text.
---- @param opts UndoGlow.HighlightRegion
-function M.highlight_region(opts)
-	local bufnr = vim.api.nvim_get_current_buf()
-
-	local state = utils.create_state(opts, M.config)
-
-	vim.schedule(function()
-		---@type UndoGlow.HandleHighlight
-		local handle_highlight_opts = {
-			bufnr = bufnr,
-			config = M.config,
-			state = state,
-			s_row = opts.s_row,
-			s_col = opts.s_col,
-			e_row = opts.e_row,
-			e_col = opts.e_col,
-		}
-
-		utils.handle_highlight(handle_highlight_opts)
-	end)
-end
+M.config = require("undo-glow.config")
 
 ---@param user_config? UndoGlow.Config
 function M.setup(user_config)
@@ -156,6 +101,129 @@ function M.setup(user_config)
 		local target = target_map[key]
 		highlights.setup_highlight(target, highlight.hl, highlight.hl_color)
 	end
+end
+
+------- Public API -------
+
+--- Undo command that highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.undo(opts)
+	local commands = require("undo-glow.commands")
+	return commands.undo(opts)
+end
+
+--- Redo command that highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.redo(opts)
+	local commands = require("undo-glow.commands")
+	return commands.redo(opts)
+end
+
+--- Yank command that highlights.
+--- For autocmd usage only.
+---@param opts? UndoGlow.CommandOpts
+function M.yank(opts)
+	local commands = require("undo-glow.commands")
+	return commands.yank(opts)
+end
+
+--- Paste below command with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.paste_below(opts)
+	local commands = require("undo-glow.commands")
+	return commands.paste_below(opts)
+end
+
+--- Paste above command with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.paste_above(opts)
+	local commands = require("undo-glow.commands")
+	return commands.paste_above(opts)
+end
+
+--- Search next command with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.search_next(opts)
+	local commands = require("undo-glow.commands")
+	return commands.search_next(opts)
+end
+
+--- Search prev command with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.search_prev(opts)
+	local commands = require("undo-glow.commands")
+	return commands.search_prev(opts)
+end
+
+--- Search prev command with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.search_star(opts)
+	local commands = require("undo-glow.commands")
+	return commands.search_star(opts)
+end
+
+--- Comment with `gc` in `n` and `x` mode with highlights.
+--- Requires `expr` = true in ``vim.keymap.set`
+---@param opts? UndoGlow.CommandOpts
+function M.comment(opts)
+	local commands = require("undo-glow.commands")
+	return commands.comment(opts)
+end
+
+--- Comment with `gc` in `o` mode. E.g. gcip, gcap, etc with highlights.
+---@param opts? UndoGlow.CommandOpts
+function M.comment_textobject(opts)
+	local commands = require("undo-glow.commands")
+	return commands.comment_textobject(opts)
+end
+
+--- Comment lines with `gcc` with highlights.
+--- Requires `expr` = true in ``vim.keymap.set`
+---@param opts? UndoGlow.CommandOpts
+function M.comment_line(opts)
+	local commands = require("undo-glow.commands")
+	return commands.comment_line(opts)
+end
+
+--- Core API to highlight changes in the current buffer.
+---@param opts? UndoGlow.HighlightChanges|UndoGlow.CommandOpts
+function M.highlight_changes(opts)
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	local state = require("undo-glow.utils").create_state(opts, M.config)
+
+	vim.api.nvim_buf_attach(bufnr, false, {
+		on_bytes = function(...)
+			return require("undo-glow.callback").on_bytes_wrapper(
+				state,
+				M.config,
+				...
+			)
+		end,
+	})
+end
+
+--- Core API to highlight a specified region in the current buffer.
+--- @param opts UndoGlow.HighlightRegion
+function M.highlight_region(opts)
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	local state = require("undo-glow.utils").create_state(opts, M.config)
+
+	vim.schedule(function()
+		---@type UndoGlow.HandleHighlight
+		local handle_highlight_opts = {
+			bufnr = bufnr,
+			config = M.config,
+			state = state,
+			s_row = opts.s_row,
+			s_col = opts.s_col,
+			e_row = opts.e_row,
+			e_col = opts.e_col,
+		}
+
+		require("undo-glow.utils").handle_highlight(handle_highlight_opts)
+	end)
 end
 
 return M
