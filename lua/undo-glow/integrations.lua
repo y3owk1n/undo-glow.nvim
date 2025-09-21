6c993c4b09f6343e3bcf2e0e5e4ead4d325bea16
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-09-21 14:10:55 +0800

    feat(integrations): add `flash.nvim` integration

--- lua/undo-glow/integrations.lua
+++ lua/undo-glow/integrations.lua
@@ -45,4 +45,46 @@ function M.substitute.action(action, opts)
 	action()
 end
 
+M.flash = {}
+
+---Flash.nvim jump command that highlights.
+---@param flash_opts? table The flash jump options.
+---@param opts? UndoGlow.CommandOpts Optional command option
+---@return nil
+function M.flash.jump(flash_opts, opts)
+	local flash_ok, flash = pcall(require, "flash")
+	if not flash_ok then
+		vim.notify(
+			"[UndoGlow] Flash.nvim is not installed",
+			vim.log.levels.ERROR
+		)
+		return
+	end
+
+	flash_opts = flash_opts or {}
+
+	if type(flash_opts) ~= "table" then
+		vim.notify(
+			"[UndoGlow] Flash action must be a table, learn more from the plugin's README",
+			vim.log.levels.ERROR
+		)
+		return
+	end
+
+	vim.g.ug_ignore_cursor_moved = true
+
+	flash.jump(flash_opts)
+
+	vim.defer_fn(function()
+		local region = require("undo-glow.utils").get_current_cursor_row()
+
+		local undo_glow_opts =
+			require("undo-glow.utils").merge_command_opts("UgSearch", opts)
+
+		require("undo-glow").highlight_region(
+			vim.tbl_extend("force", undo_glow_opts, region)
+		)
+	end, 5)
+end
+
 return M
